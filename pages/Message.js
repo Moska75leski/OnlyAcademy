899ef2d9be3cb8eaf24f26235ba37ceb519686d5
EVERTON MOSCALESKI <91913754+Moska75leski@users.noreply.github.com>
2024-05-13 20:39:32 -0300

    Atualização v1

---
+++ pages/Message.js
@@ -0,0 +1,33 @@
+// src/pages/Message.js
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const Message = ({ sender, content, time }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.sender}>{sender}</Text>
+      <Text>{content}</Text>
+      <Text style={styles.time}>{time}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 8,
+    marginVertical: 4,
+    borderRadius: 8,
+    backgroundColor: "#e0e0e0",
+  },
+  sender: {
+    fontWeight: "bold",
+    marginBottom: 4,
+  },
+  time: {
+    fontSize: 12,
+    color: "gray",
+    alignSelf: "flex-end",
+  },
+});
+
+export default Message;
